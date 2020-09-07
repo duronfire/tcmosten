e24ba3c35b0fe1de1eda9c149cd5b2f1ce70e77b
Wentao Zhu <wz@cfd-schuck.de>
2020-09-08 00:30:28 +0200

    dummytest form

--- tcmosten/isys/models/dummy.py
+++ tcmosten/isys/models/dummy.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+from django.forms import ModelForm
 # Create your models here.
 
 class Dummy(models.Model):
@@ -7,3 +9,10 @@ class Dummy(models.Model):
 #    nachname = models.CharField(max_length=30)
     ckanton=(('SG','St.Gallen'),('TG','Thurgau'),('AI','Appenzell Innerrhoden'),('AR','Appenzell Ausserrhoden'),('ZH','Zürich'),('BE','Bern'),('UR','Uri'),('SZ','Schwyz'),('OW','Obwalden'),('NW','Nidwalden'),('GL','Glarus'),('BL','Basel-Landschaft'),('BS','Basel-Stadt'),('SO','Solothurn'),('FR','Freiburg'),('ZG','Zug'),('SH','Schaffhausen'),('GR','Graubünden'),('AG','Aargau'),('JU','Jura'),('GE','Genf'),('NE','Neuenburg'),('VS','Wallis'),('VD','Waadt'),('TI','Tessin'))
     kanton = models.CharField(max_length=15,choices=ckanton,default='SG', null=True,blank=True)   # The default value is used when new model instances are created and a value isn’t provided for the field.
+
+
+class Dummyform(ModelForm):
+    class Meta:
+        model = Dummy
+        fields = '__all__'
+
--- tcmosten/isys/views.py
+++ tcmosten/isys/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-from .models.dummy import Dummy
+from .models.dummy import Dummy,Dummyform
 
-from django.template import loader
+
 # Create your views here.
 def index(request):
     all=Dummy.objects.all()
     name0=[i.vorname for i in all]
-    template = loader.get_template('isys/index.html')
-    context={'testvar':name0}
-    return HttpResponse(template.render(context, request))
+
+    if request.method=='GET':
+        form=Dummyform()
+        return render(request, 'isys/index.html', {'testvar':name0, 'form': form})
+
+    elif request.method=='POST':
+        form=Dummyform(request.POST)
+        return HttpResponse('yes!')
+
+
+        
+
+    
