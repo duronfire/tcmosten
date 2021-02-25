181c20a453fe4ea67e6ad7e153d52e9c6ce19791
wentao zhu <z.tomcat@gmail.com>
2021-02-25 13:35:28 -0800

    update

--- tcmosten/tcmosten/settings.py
+++ tcmosten/tcmosten/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'klemmrz0@#qdj60z(27_b(k4b-i-5fj+m&k128xj%mjt$ga*25'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.136.128']
 
 
 # Application definition
